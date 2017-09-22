0cff75dcf8b8e121cecba79c6a28737f85f1e83e
EdCollinson1502 <31353239+EdCollinson1502@users.noreply.github.com>
2017-09-22 08:55:35 -0700

    final commit

--- DemocracyBot.java
+++ DemocracyBot.java
@@ -10,15 +10,70 @@ public class DemocracyBot extends Robot
     public DemocracyBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
-    
     public void fixBallots() {
         move();
-        if(!nextToABeeper()){
-            
+        for (int i = 0; i < 5;  i++){
+            if(!nextToABeeper()){
+               faceNorth();
+               move();
+               if(nextToABeeper()){
+                   pickBeeper();
+               }
+               faceSouth();
+               move();
+               move();
+               if(nextToABeeper()){
+                   pickBeeper();
+               }
+               faceNorth();
+               move();
+               faceEast();
+            }
+            else{
+               faceNorth();
+               move();
+               if(!nextToABeeper()){
+                   putBeeper();
+               }
+               faceSouth();
+               move();
+               move();
+               if(!nextToABeeper()){
+                   putBeeper();
+               }
+               faceNorth();
+               move();
+               faceEast();
+            }
+            move();
+            move();
         }
-        move();
-        move();
+        
     }
-   
+    public void faceEast(){
+        while(!facingEast()){
+                turnLeft();
+        }
+    }
+    public void faceSouth(){
+        while(!facingSouth()){
+                turnLeft();
+        }
+    }
+    public void faceNorth(){
+        while(!facingNorth()){
+                turnLeft();
+        }
+    }
+    public void faceWest(){
+        while(!facingWest()){
+                turnLeft();
+        }
+    }
+    public void turnRight(){
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
 }
 
